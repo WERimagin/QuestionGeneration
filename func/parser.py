50dedf20f40e38e899f29c6d3a3fc9b49f2b61eb
笹沢裕一 <werimagin@gmail.com>
2019-03-24 14:21:20 +0900

    add

--- func/parser.py
+++ func/parser.py
@@ -22,7 +22,7 @@ def get_args():
     parser.add_argument("--epoch_num", type=int, default="200", help="input epoch number")
     parser.add_argument("--dropout", type=float, default="0.3", help="input epoch number")
     parser.add_argument("--layer_size", type=int, default="2", help="input epoch number")
-    parser.add_argument("--vocab_size", type=int, default="10000", help="input epoch number")
+    parser.add_argument("--vocab_size", type=int, default="50000", help="input epoch number")
     parser.add_argument("--lr", type=float, default="0.001", help="input epoch number")
     parser.add_argument("--teacher_rate", type=float, default="0.5", help="input epoch number")
 
